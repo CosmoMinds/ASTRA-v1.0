e9c1699ff5be08048011c02fc7868bd8dcbe42f9
CosmoMinds <your-cosmominds198@gmail.com>
2026-08-26 01:10:57 +0530

    Initial ASTRA v1.0 prototype

---
+++ main.py
@@ -0,0 +1,18 @@
+print("=========================================")
+print("         Astra v1.0")
+print("         Satellite Detection System")
+print("=========================================")
+
+name = input("Enter your name: ")
+
+print()
+print("Welcome to Astra's Code Interface, ", name)
+print("System Initialised Successfully.")
+
+satellite_count = 10
+system_version = 1.0
+tracking_enabled = True
+print("Status:  ")
+print("Total Satellites - ", satellite_count, type(satellite_count))
+print("Current Version - ", system_version, type(system_version))
+print("Tracking Status Enabled - ", tracking_enabled, type(tracking_enabled))
