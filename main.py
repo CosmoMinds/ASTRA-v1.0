398f7fa909980aa654c5ea05026b68b573fbe064
CosmoMinds <your-cosmominds198@gmail.com>
2026-08-26 10:56:57 +0530

    Basic Authentication System

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ print("         Astra v1.0")
 print("         Satellite Detection System")
 print("=========================================")
 
-name = input("Enter your name: ")
-
-print()
-print("Welcome to Astra's Code Interface, ", name)
-print("System Initialised Successfully.")
-
-satellite_count = 10
-system_version = 1.0
-tracking_enabled = True
-print("Status:  ")
-print("Total Satellites - ", satellite_count, type(satellite_count))
-print("Current Version - ", system_version, type(system_version))
-print("Tracking Status Enabled - ", tracking_enabled, type(tracking_enabled))
+def login():
+    username = input("Username: ")
+    password = input("Password: ")
+    if username == "Shriram" and password == "Astra@0324":
+        print("Login Successful")
+        return True
+    else:
+        print("Invalid Login credentials")
+        return False
+while True:
+    if login():
+        print("Opening Astra interface...")
+        break
+    else:
+        print("Try Again.")
