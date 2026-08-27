8dca206e258762cccc0e61ca5266e3609f50b72e
CosmoMinds <your-cosmominds198@gmail.com>
2026-08-27 19:57:47 +0530

    Complete satellite database search and selection

--- main.py
+++ main.py
@@ -20,6 +20,12 @@ def load_satellites():
     with open("data/satellites.json", "r") as file:
         satellites = json.load(file)
     return satellites
+def search_satellite(satellites, search_term):
+    results = []
+    for satellite in satellites:
+        if search_term.lower() in satellite["name"].lower():
+            results.append(satellite)
+    return results
 
 def dashboard():
     while True:
@@ -41,34 +47,86 @@ def dashboard():
         if choice == "1":
             satellites = load_satellites()
 
-            print()
-            print("=============================================")
-            print("                     Satellite Database")
-            print("=============================================")
-            for index, satellite in enumerate(satellites, start = 1):
-                print(f"{index}. {satellite['name']}")
-            print("=============================================")
-            selection = input("Enter the satellite number to view the details: ")
-            if selection.isdigit():
-                selection = int(selection)
-                if 1 <= selection <= len(satellites):
-                    satellite = satellites[selection - 1]
+            while True:
+                print()
+                print("=====================================================")
+                print("                     Satellite Database")
+                print("=====================================================")
+                print("1. View All Satellites")
+                print("2. Search Satellites")
+                print("0. Back to Dashboard")
+                database_choice = input ("Enter your choice: ")
+                if database_choice == "1":
                     print()
-                    print("=============================================")
-                    print("                     Satellite Details")
-                    print("=============================================")
-                    print("Name     :", satellite["name"])
-                    print("NORAD ID     :", satellite["norad_id"])
-                    print("Launch Date      :", satellite["launch_date"])
-                    print("Orbit        :", satellite["orbit"])
-                    print("Mission      :", satellite["mission"])
-                    print("Status       :", satellite["status"])
-                    print("=============================================")
-                    input ("Press Enter to return to the dashboard")
+                    print("=================================================")
+                    print("                        All Satellites")
+                    print("=================================================")
+                    for index, satellite in enumerate(satellites, start = 1):
+                        print(f"{index}. {satellite['name']}")
+                    selection = input("\n Enter Satellite number to view details: ")
+                    if selection.isdigit():
+                        selection = int(selection)
+                        if 1 <= selection <= len(satellites):
+                            satellite = satellites[selection - 1]
+                            print()
+                            print("=========================================")
+                            print("                 Satellite Details")
+                            print("=========================================")
+                            print("Name                 :", satellite["name"])
+                            print("NORAD ID             :", satellite["norad_id"])
+                            print("Launch Date          :", satellite["launch_date"])
+                            print("Launch Vehicle       :", satellite["launch_vehicle"])
+                            print("Orbit                :", satellite["orbit"])
+                            print("Mission              :", satellite["mission"])
+                            print("Status               :", satellite["status"])
+                            print("=========================================")
+                            input("Press Enter to continue")
+                        else:
+                            print("Invalid Satellite Number.")
+                    else:
+                        print("Please enter a valid number.")
+                elif database_choice == "2":
+                    search_term = input("Enter Satellite Name: ")
+                    results = search_satellite(satellites, search_term)
+
+                    print()
+                    print("=======================================================")
+                    print("                   Search Results")
+                    print("=======================================================")
+
+                    if results:
+                        for index, satellite in enumerate(results, start = 1):
+                            print(f"{index}. {satellite['name']}")
+                        selection = input("\nEnter Satellite number to view details: ")
+                        if selection.isdigit():
+                            selection = int(selection)
+                            if 1 <= selection <= len(results):
+                                satellite = results[selection - 1]
+                                print()
+                                print("==========================================")
+                                print("             Satellite Details")
+                                print("==========================================")
+
+                                print("Name                 :", satellite["name"])
+                                print("NORAD ID             :", satellite["norad_id"])
+                                print("Launch Date          :", satellite["launch_date"])
+                                print("Launch Vehicle       :", satellite["launch_vehicle"])
+                                print("Orbit                :", satellite["orbit"])
+                                print("Mission              :", satellite["mission"])
+                                print("Status               :", satellite["status"])
+                                print("=========================================")
+                                input("Press Enter to continue")
+                            else:
+                                print("Invalid Satellite Number.")
+                        else:
+                            print("Please enter a valid number.")
+                    else:
+                        print("No satellite found.")
+                        input("Press Enter to continue...")
+                elif database_choice == "0":
+                    break
                 else:
-                    print("Invalid Satellite Number")
-            else:
-                print("Enter a Valid Number.")
+                    print("Invalid Choice")
 
         elif choice == "2":
             print("Opening Real-time Tracking...")
